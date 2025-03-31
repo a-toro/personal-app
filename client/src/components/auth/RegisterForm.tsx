ae6de35e352a6553e87847230f07cc2f4d2d4900
wtoroben <william.toro@indesap.com>
2025-03-31 13:49:48 -0500

    add formik for validte inputs

--- client/src/components/auth/RegisterForm.tsx
+++ client/src/components/auth/RegisterForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -9,8 +10,25 @@ import {
 } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useFormik } from "formik";
+// import * as Yup from "yup";
+
+import { Eye, EyeOff } from "lucide-react";
 
 export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    // validationSchema: {},
+    onSubmit: (values) => {
+      console.log({ values });
+    },
+  });
   return (
     <Card className="w-full sm:w-[380px]">
       <CardHeader>
@@ -19,24 +37,70 @@ export default function RegisterForm() {
           Crea una cuenta para llevar el control de tus gastos
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-col gap-y-3">
-        <div className="w-full space-y-2">
-          <Label htmlFor="name">Email</Label>
-          <Input placeholder="Ingresa tu email" id="email" />
-        </div>
+      <CardContent>
+        <form className="flex flex-col gap-y-3" onSubmit={formik.handleSubmit}>
+          <div className="w-full space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              placeholder="Ingresa tu email"
+              id="email"
+              name="email"
+              type="email"
+              required
+              value={formik.values.email}
+              onChange={formik.handleChange}
+            />
+          </div>
 
-        {/* <div className="w-full space-y-2">
-          <Label htmlFor="name">Contraseña</Label>
-          <Input placeholder="Nombre" id="name" />
-        </div> */}
+          <div className="w-full space-y-2">
+            <Label htmlFor="password">Contraseña</Label>
+            <div className="flex items-center gap-1">
+              <Input
+                placeholder="Ingresa tu contraseña"
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              <Button
+                type="button"
+                variant="outline"
+                size={"icon"}
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <EyeOff /> : <Eye />}
+              </Button>
+            </div>
+          </div>
 
-        {/* <div className="w-full space-y-2">
-          <Label htmlFor="name">Confirmar Contraseña</Label>
-          <Input placeholder="Nombre" id="name" />
-        </div> */}
+          <div className="w-full space-y-2">
+            <Label htmlFor="name">Confirmar Contraseña</Label>
+            <div className="flex items-center gap-1">
+              <Input
+                placeholder="Confirma tu contraseña"
+                id="confirmPassword"
+                name="confirmPassword"
+                type={showConfirmPassword ? "text" : "password"}
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+              />
+              <Button
+                type="button"
+                variant="outline"
+                size={"icon"}
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+              >
+                {showConfirmPassword ? <EyeOff /> : <Eye />}
+              </Button>
+            </div>
+          </div>
+          <Button type="submit" className="w-full mt-2">
+            Registrarme
+          </Button>
+        </form>
       </CardContent>
       <CardFooter className="flex flex-col w-full gap-y-4">
-        <Button className="w-full">Registrarme</Button>
         <p className="text-sm text-slate-400 hover:text-slate-700">
           ¿Olvido su Contraseña?
         </p>
