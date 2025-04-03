838e1100c85119db3845e8dc34c04385ff92c983
wtoroben <william.toro@indesap.com>
2025-04-03 10:02:13 -0500

    remove not use imported

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Loading() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-slate-300 opacity-75 z-40 text-black">
