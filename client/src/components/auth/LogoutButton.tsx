68148b4df689572f97be185168d470899b2143f2
wtoroben <william.toro@indesap.com>
2025-04-03 10:46:21 -0500

    add icon

--- client/src/components/auth/LogoutButton.tsx
+++ client/src/components/auth/LogoutButton.tsx
@@ -1,8 +1,14 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
 
 export default function LogoutButton() {
   const { logout } = useAuth();
 
-  return <Button onClick={() => logout()}>Logout</Button>;
+  return (
+    <Button variant="outline" className="w-full" onClick={() => logout()}>
+      <LogOut />
+      Cerrar sesión
+    </Button>
+  );
 }
