0b8d8a781d38a421aa2704a471202c79727c71d3
wtoroben <william.toro@indesap.com>
2025-04-03 10:01:40 -0500

    Add lazy to MainLayout and Not Found route

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,9 +2,10 @@
 import { Route, Routes } from "react-router";
 import RequiereAuth from "./RequiereAuth";
 import { lazy } from "react";
-import MainLayout from "@/layouts/Layout";
+// import MainLayout from "@/layouts/Layout";
 
 const AuthPage = lazy(() => import("@/pages/AuthPage"));
+const MainLayout = lazy(() => import("@/layouts/Layout"));
 
 const ROLES = {
   Admin: "ADMIN",
@@ -21,7 +22,10 @@ export default function AppRoutes() {
         >
           <Route path="/" index element={<h1>Home</h1>}></Route>
         </Route>
-        <Route path="*" element={<h1 className="text-2xl font-semibold">Not found page</h1>} />
+        <Route
+          path="*"
+          element={<h1 className="text-2xl font-semibold">Not found page</h1>}
+        />
       </Route>
     </Routes>
   );
