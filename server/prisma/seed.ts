c47573132bb456902a02b2e92dd30eacaed2fe33
atoro <wtorobenavides@gmail.com>
2025-04-20 12:09:40 -0500

    chore: add seed data on init

---
+++ server/prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, Role } from "@prisma/client";
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+
+const prisma = new PrismaClient();
+async function main() {
+    await prisma.user.create({
+        data: {
+            name: "William Toro",
+            email: process.env.FIRST_ADMIN_EMAIL as string,
+            password: process.env.FIRST_ADMIN_PASSWORD as string,
+            role: Role.ADMIN
+        }
+    });
+
+    await prisma.user.create({
+        data: {
+            name: "William Toro",
+            email: "test@utp.edu.co",
+            password: "123456",
+            role: Role.USER
+        }
+    });
+
+}
+
+main()
+    .then(() => {
+        console.log("Seeding data succesfully")
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    })
+    .finally(() => prisma.$disconnect());
